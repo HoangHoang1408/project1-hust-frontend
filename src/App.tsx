857da6250b8fbabfe97cb7e4032061122d0c2890
Hoang1408 <hoang1482002@gmail.com>
2022-08-24 21:46:47 +0700

    init app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h1 className="font-bold text-blue-600 text-2xl">Hello from Hoang</h1>
+    </div>
+  );
+}
+
+export default App;
