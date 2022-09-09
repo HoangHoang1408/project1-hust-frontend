9a984cfccdd160f6c7f6ecad0e62acce3fcb3014
Hoang1408 <hoang1482002@gmail.com>
2022-09-09 08:22:37 +0700

    fix login protect

--- src/layouts/LoginProtect.tsx
+++ src/layouts/LoginProtect.tsx
@@ -1,18 +1,19 @@
 import { useReactiveVar } from "@apollo/client";
 import { Fragment, useEffect } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
-import { userVar } from "../apollo/reactiveVar/loginStatus";
+import { loginStatusVar, userVar } from "../apollo/reactiveVar/loginStatus";
 
 type Props = {};
 
 const LoginProtect = (props: Props) => {
   const user = useReactiveVar(userVar);
+  const loginStatus = useReactiveVar(loginStatusVar);
   const navigate = useNavigate();
   useEffect(() => {
-    if (!user) {
+    if (loginStatus && !loginStatus.isLoggedIn) {
       navigate("/");
     }
-  }, [user]);
+  }, [loginStatus]);
   return (
     <Fragment>
       <Outlet />
